31012f6ce198167eec177988ffdb0a19e24c5e5b
nhutnamhcmus <sakura9x016@gmail.com>
2022-04-26 11:06:16 +0700

    update

--- main.py
+++ main.py
@@ -52,3 +52,69 @@ def parallel_scan(a):
 in_arr = np.array([1, 2, 3, 4, 5, 6, 7, 8])
 res = parallel_scan(in_arr)
 print(res)
+
+
+def get_neutral_element(op='+', tp='scalar'):
+    if op == '+':
+        if tp == 'scalar':
+            return 0
+        if tp == 'matrix':
+            return np.identity(size)
+    elif op == '*':
+        if tp == 'scalar':
+            return 1
+
+def associative_op(a, b, op='+'):
+    if op == '+':
+        return a + b
+    elif op == '*':
+        return a * b
+    elif op == '@':
+        return a @ b
+
+pad_power2 = lambda x, op='+' : np.pad(x, (get_neutral_element(op), int(2**np.ceil(np.log2(len(x)))) - len(x)), 'constant')
+
+def parallel_scan2(a, op='+'):
+    # Save the input:
+    T = a.size
+    if T & (T-1) != 0 or T == 0:
+        cT = a.size
+        a = pad_power2(a, op)
+    else:
+        cT = T
+
+    b = np.copy(a)
+    T = a.size
+    bound_sweep = int(np.log2(T))
+    # Up sweep
+    for d in range(0, bound_sweep):
+        step = 2**(d+1)
+        for i in range(0, T-1, step):
+            j = i + 2**d - 1
+            k = i + 2**(d+1) - 1
+            #a[k] = a[j] + a[k]
+            a[k] = associative_op(a[j], a[k], op)
+
+    # 0 is actually the neutral element for associative operator
+    a[T-1] = get_neutral_element(op)
+    # Down sweep
+    for d in range(bound_sweep-1, -1, -1):
+        step = 2**(d+1)
+        for i in range(0, T-1, step):
+            j = i + 2**d - 1
+            k = i + 2**(d+1) - 1
+            t = a[j]
+            a[j] = a[k]
+            #a[k] = a[k] + t
+            a[k] = associative_op(a[k], t, op)
+
+    # Final pass
+    out = []
+    for i in range(0, cT):
+        #out.append(a[i] + b[i])
+        out.append(associative_op(a[i], b[i], op))
+    return np.array(out)
+
+in_arr = np.array([1, 2, 3, 4, 5])
+res = parallel_scan2(in_arr)
+print(res)
